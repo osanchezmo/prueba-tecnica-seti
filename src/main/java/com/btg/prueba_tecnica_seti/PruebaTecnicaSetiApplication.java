2478c97024f59e906527ec252d23d54f1696c524
Oscar Sanchez <oscar.sanchezmoli.07@gmail.com>
2026-03-18 10:35:01 -0500

    chore:proyecto incial

---
+++ src/main/java/com/btg/prueba_tecnica_seti/PruebaTecnicaSetiApplication.java
@@ -0,0 +1,13 @@
+package com.btg.prueba_tecnica_seti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaSetiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaSetiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/btg/prueba_tecnica_seti/PruebaTecnicaSetiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btg.prueba_tecnica_seti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaSetiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
